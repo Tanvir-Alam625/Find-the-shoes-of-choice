7c7897d6b997c8dbe2cf9e476ea18b394f969232
tanveer <mdtanviralamht731@gmail.com>
2022-03-26 13:36:52 +0600

    product compoment done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './Product.css';
-const Product = () => {
+const Product = ({data}) => {
+    const {name, price, img}=data
+
     return (
         <div className='product'>
+        <div className="card">
+            <img src={img} alt="product" className="product-image"/>
+            <div className="card-info">
+                <h3>{name}</h3>
+                <p>Price: ${price}</p>
+                <button>
+                    ADD TO CART
+                </button>
+            </div>
+        </div>
         </div>
     );
 };
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Main.css'
 const Main = () => {
+    const [prodcuts, setProducts]=useState([]);
+
+    useEffect(()=>{
+        fetch('product.json')
+        .then(res=> res.json())
+        .then(data=> setProducts(data))
+    } ,[])
     return (
         <div className='main'>
             <div className="product-container">
-            <Product/>
+            {
+                 prodcuts.map(product=> <Product key={product.id} data={product}/> )
+            }
             </div>
             <div className="cart-container">
                 <h2>this cart</h2>
