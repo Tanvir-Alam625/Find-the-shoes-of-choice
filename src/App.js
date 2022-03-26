483eeacdc8469005b511e8623561f34755d07ddb
tanveer <mdtanviralamht731@gmail.com>
2022-03-26 12:20:21 +0600

    Header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div> 
+    <Header/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>Find the shoes of choice</h2>
+            <h4>Choose 4 Clothes</h4>
+        </div>
+    );
+};
+
+export default Header;
